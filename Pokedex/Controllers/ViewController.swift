c0eb8672a08f1048f5f5f17507e7ac4db9b6431f
Tyler Eaden <tyray402@yahoo.com>
2024-09-22 17:07:32 -0400

    Added logo ImageView to OpeningView on storyboard

---
+++ Pokedex/Controllers/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Tyler Eaden on 9/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    lazy var pokemonModel = PokemonModel.shared
+
+    @IBOutlet weak var logo: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
