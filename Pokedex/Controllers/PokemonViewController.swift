71d4c97c968f7638f7ccfceeb4789c50378949e6
Tyler Eaden <tyray402@yahoo.com>
2024-09-24 03:23:13 -0400

    Added Timer and animation to PokemonViewController that swaps images

--- Pokedex/Controllers/PokemonViewController.swift
+++ Pokedex/Controllers/PokemonViewController.swift
@@ -12,6 +12,8 @@ class PokemonViewController: UIViewController {
     
     var pokemonIndex: Int?
     
+    lazy var currentImageIndex = 0
+    
     lazy var pokemon: SinglePokemonModel = {
         guard let index = pokemonIndex, let result = try? PokemonModel.getPokemonByIndex(index: index) else {
             fatalError("PokemonViewController: Could not retrieve pokemon by index")
@@ -19,8 +21,8 @@ class PokemonViewController: UIViewController {
         return result
     }()
 
-    lazy var artwork: UIImage = {
-        guard let index = pokemonIndex, let result = try? PokemonModel.getArtworkByIndex(index: index) else {
+    lazy var artwork: [UIImage] = {
+        guard let index = pokemonIndex, let result = try? PokemonModel.getAllArtworkByIndex(index: index) else {
             fatalError("PokemonViewController: Could not retrieve image by index")
         }
         return result
@@ -31,6 +33,12 @@ class PokemonViewController: UIViewController {
             fatalError("PokemonViewController: Could not retrieve audoi by index")
         }
         return result
+    }()
+    
+    lazy var timer: Timer = {
+        return Timer.scheduledTimer(withTimeInterval: 2.0, repeats: true) { [weak self] _ in
+            self?.animateImageTransition()
+        }
     }()
 
     @IBOutlet weak var imageView: UIImageView!
@@ -47,8 +55,11 @@ class PokemonViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        imageView.image = artwork
+        imageView.image = artwork[0]
         self.setupAudioPlayer()
+        
+        // Start the timer
+        startImageCycling()
     }
     
     private func setupAudioPlayer() {
@@ -58,5 +69,25 @@ class PokemonViewController: UIViewController {
         volumeSlider.minimumValue = 0.0
         volumeSlider.maximumValue = 1.0
         volumeSlider.value = sound.volume
+    }
+    
+    func startImageCycling() {
+        timer = Timer.scheduledTimer(withTimeInterval: 2.0, repeats: true) { [weak self] _ in // Adjust interval as needed
+            self?.animateImageTransition()
+        }
+    }
+    
+    func animateImageTransition() {
+
+        // Toggle between the two images
+        self.currentImageIndex = (self.currentImageIndex + 1) % self.artwork.count
+
+        UIView.transition(
+            with: imageView,
+            duration: 0.5,
+            options: .transitionCrossDissolve,
+                          animations: {
+                              self.imageView.image = self.artwork[self.currentImageIndex]
+                          }, completion: nil)
     }
 }
